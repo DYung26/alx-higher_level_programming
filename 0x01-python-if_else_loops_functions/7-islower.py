92b6ccf99c61c1d044f66337b916e9712826938f
DYung26 <theadstore1407@gmail.com>
2024-01-03 04:35:44 -0800

    'islower': A function that checks for lowercase character.

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if c in 'abcdefghijklmnopqrstuvwxyz':
+    if c in "abcdefghijklmnopqrstuvwxyz":
         return True
-    elif c in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+    elif c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
         return False
     else:
         return False
