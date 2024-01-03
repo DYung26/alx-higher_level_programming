57cef1d5bcf594d3e1756760ccd646e108dd0cf7
DYung26 <theadstore1407@gmail.com>
2024-01-03 05:59:33 -0800

    FizzBuzz!

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             msg = "FizzBuzz "
-        if i % 3 == 0:
+        elif i % 3 == 0:
             msg = "Fizz "
         elif i % 5 == 0:
             msg = "Buzz "
