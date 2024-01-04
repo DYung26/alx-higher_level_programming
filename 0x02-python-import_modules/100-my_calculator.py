878482b7c1dd46d00582fec00451fc5fcf39bc14
DYung26 <theadstore1407@gmail.com>
2024-01-04 10:32:49 -0800

    Build my own calculator! - A program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -16,8 +16,8 @@ if __name__ == "__main__":
     if operator == '+':
         print("{} + {} = {}".format(a, b, add(a, b)))
     elif operator == '-':
-        print("{} + {} = {}".format(a, b, sub(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == '*':
-        print("{} + {} = {}".format(a, b, mul(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
     elif operator == '/':
-        print("{} + {} = {}".format(a, b, div(a, b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
