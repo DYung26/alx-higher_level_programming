4a5b1d4d583f5db7ff827a631f5067781fc7f2e3
DYung26 <theadstore1407@gmail.com>
2024-01-03 18:51:32 -0800

    Create 'lists.h'

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,15 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
@@ -18,8 +16,9 @@ typedef struct listint_s
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
