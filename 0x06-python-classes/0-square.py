87136f43aa4660468042632649469f458ec5623e
DYung26 <theadstore1407@gmail.com>
2024-01-23 09:05:55 -0800

    an empty class Square that defines a square.

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Attributes:
+Attributes: None
 """
 class Square():
     """
-    Args:
-    Attributes:
+    Args: None
+    Attributes: None
     """
     pass
