2773d9a91cd9d314cbd72605563553a4a1688910
DYung26 <theadstore1407@gmail.com>
2024-01-23 18:39:02 -0800

    A class Square that defines a square by: (based on 5-square.py)
    NEW:
    
        * Private instance attribute: position:
    
            + property def position(self): to retrieve it
    
            + property setter def position(self, value): to set it:
    
                - position must be a tuple of 2 positive integers, otherwise raise a TypeError exception with the message position must be a tuple of 2 positive integers
    
        * Instantiation with optional size and optional position: def __init__(self, size=0, position=(0, 0)):
    
            NEW:
    
            + position should be use by using space - Don’t fill lines by spaces when position[1] > 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -17,7 +17,7 @@ class Square:
     """
     def __init__(self, size=0, position=(0, 0)):
         self.__size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
