48353166ee393c3ebec03561c01e38daeef8cc0b
DYung26 <theadstore1407@gmail.com>
2024-01-23 17:06:06 -0800

    A class Square that defines a square by: (based on 4-square.py)
    * Private instance attribute: size:
    
        + property def size(self): to retrieve it
    
        + property setter def size(self, value): to set it:
    
            - size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
    
            - if size is less than 0, raise a ValueError exception with the message size must be >= 0
    
    * Instantiation with optional size: def __init__(self, size=0):
    
    * Public instance method: def area(self): that returns the current square area
    
    * Public instance method: def my_print(self): that prints in stdout the square with the character #:
    
        + if size is equal to 0, print an empty line

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+# -*- coding_ utf-8 -*-
+"""
+Args: None
+Attributes: None
+"""
+
+
+class Square:
+    """
+    Args:
+        size (int): lenght of square
+    Attributes:
+        size (int): lenght of square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Returns:
+            size
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Returns:
+            Area
+        """
+        return self.__size**2
+
+    def my_print(self):
+        """
+    
+        """
+        if self.__size == 0:
+            print("")
+        else:
+            j = 0
+            while j < self.__size:
+                i = 0
+                while i < self.__size:
+                    print("#", end="")
+                    i+=1
+                j+=1
+                print("")
