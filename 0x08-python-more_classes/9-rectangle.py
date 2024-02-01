5db27ca51025f9509bc845106027ae6f28cdbdcd
DYung26 <theadstore1407@gmail.com>
2024-01-31 19:48:36 -0800

    a class Rectangle that defines a rectangle by: (based on 8-rectangle.py)

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -169,10 +169,9 @@ class Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() == rect_2.area() or rect_1.area() > rect_2.area():
+        if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
             return rect_1
-        if rect_1.area() < rect_2.area():
-            return rect_2
+        return rect_2
 
     @classmethod
     def square(cls, size=0):
