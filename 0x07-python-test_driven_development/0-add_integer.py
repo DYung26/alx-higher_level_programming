2e49df903df1d163f173163b9d707aef1a5bc0dc
DYung26 <theadstore1407@gmail.com>
2024-02-02 14:33:09 -0800

    added doctests for the add_integer function in '0-add_integer.py'

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,26 +1,13 @@
 #!/usr/bin/python3
-"""
-Defines the add_integer function.
-
+"""Defines the add_integer function.
 Functions:
     add_integer(a, b=98): Adds two numbers and returns the result.
-
 If executed as a standalone script, it runs doctests from the 0-add_integer.txt file.
 """
 def add_integer(a, b=98):
-    """
-    Adds two numbers and returns the result.
-
-    Parameters:
-        a (int or float): The first number to be added.
-        b (int or float, optional): The second number to be added. Default is 98.
-
-    Returns:
+    """Returns:
         int: The sum of the two input numbers, a and b, as an integer.
-
-    Raises:
-        TypeError: If either a or b is not an integer or a float.
-    """
+    Raises: TypeError: If either a or b is not an integer or a float."""
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
