16e0a773c64dcfcff3c9c38e0449644828cd1671
DYung26 <theadstore1407@gmail.com>
2024-02-15 20:32:25 -0800

    Updated the class 'Square' by adding the public method 'def to_dictionary(self):' that returns the dictionary representation of a 'Square'

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Square module
+"""
 from models.rectangle import Rectangle
 
 
@@ -7,17 +10,59 @@ class Square(Rectangle):
         width = size
         height = size
         super().__init__(width, height, x, y, id)
-        self.__size = size        
 
     def __str__(self):
-        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.__size}"
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
 
     @property
     def size(self):
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         attributeName = "width"
+        Rectangle._Rectangle__check(value, attributeName) # super().__check(value, attributeName)
+        self.width = value
+        attributeName = "height"
         Rectangle._Rectangle__check(value, attributeName)
-        self.__size = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """if args:
+            super().update(*args)
+        """
+        if args:
+            try:
+                self.id = args[0]
+            except IndexError:
+                pass
+            try:
+                self.size = args[1]
+            except IndexError:
+                pass
+            try:
+                self.x = args[2]
+            except IndexError:
+                pass
+            try:
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        return {
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        }
