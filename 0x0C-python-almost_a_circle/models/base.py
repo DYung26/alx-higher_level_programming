bdfed526d754b132ec3505d41eef702cd3ac6841
DYung26 <theadstore1407@gmail.com>
2024-02-16 12:12:43 -0800

    Updated the class 'Base' by adding the class method 'def save_to_file(cls, list_objs):' that writes the JSON string representation of 'list_objs' to a file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -95,7 +95,7 @@ class Base:
         """
         if list_objs:
             listObjs = [i.to_dictionary() for i in list_objs]
-            with open("Rectangle.json", "w") as file:
+            with open(f"{cls.__name__}.json", "w") as file:
                 file.write(cls.to_json_string(listObjs))
 
     @staticmethod
