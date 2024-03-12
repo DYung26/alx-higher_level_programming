16065090feb41222b5f6e13b1bcbd88b135f3830
DYung26 <theadstore1407@gmail.com>
2024-03-12 15:34:06 -0700

    A script that lists all rows of the table `first_table` from the dat abase `hbtn_0c_0` in your MySQL server.

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,2 +1,2 @@
--- Lists all rows of the table `first_table` from the database `hbtn_0c_0`
-SELECT * FROM hbtn_0c_0.first_table;
+-- Lists all rows of the table `first_table` from the database `hbtn_0c_0`.
+SELECT * FROM first_table;
