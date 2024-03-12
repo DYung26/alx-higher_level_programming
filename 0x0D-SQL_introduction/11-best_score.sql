fb4ac8dc338a281ffc2b369dd0414b83447efaa0
DYung26 <theadstore1407@gmail.com>
2024-03-12 16:32:20 -0700

    A script that lists `score` and `name` records with a `score >= 10` in the table `second_table` in descending order.

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Lists `score` and `name` records with a `score >= 10` in the table `second_table` in descending order.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
