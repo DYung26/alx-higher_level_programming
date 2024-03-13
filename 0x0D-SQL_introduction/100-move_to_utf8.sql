017d207a8a23db1ead9991eb03d058defc6dd28b
DYung26 <theadstore1407@gmail.com>
2024-03-12 19:11:15 -0700

    A script that converts `hbtn_0c_0` database to UTF8 (`utf8mb4`, coll ate `utf8mb4_unicode_ci`).

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,4 +4,3 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert the name field in the table to UTF-8
 ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
