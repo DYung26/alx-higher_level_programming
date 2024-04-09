c306ab2839355682f55a17e46b2a262a399a6210
DYung26 <theadstore1407@gmail.com>
2024-04-08 21:43:48 -0700

    Create a sentence: a script that prints two arguments passed to it, in the following format: “ is ”

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-arg1 = process.argv[2];
-arg2 = process.argv[3];
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
 
-console.log(arg1 + " is " + arg2);
+console.log(arg1 + ' is ' + arg2);
