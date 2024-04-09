561412e9f29d3ecc13d27a3bd751d17e313fa408
DYung26 <theadstore1407@gmail.com>
2024-04-09 16:28:22 -0700

    `0x13. JavaScript - Objects, Scopes and Closures`

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js');
-
 console.log(list);
-console.log(list.map((value, index) => value * index));
+console.log(list.map((item, index) => item * index));
