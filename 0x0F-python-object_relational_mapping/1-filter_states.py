ec23eca232620ac0f508da05bc54234532d14cb6
DYung26 <theadstore1407@gmail.com>
2024-04-15 05:59:44 -0700

    a script that lists all `states` with a `name` starting with `N` (upper N) from the database `hbtn_0e_0_usa`.

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Filter states'''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    '''Establish a connection to the MySQL database.
+    Parameters:
+        host: The host name where the MySQL server is running.
+        port: The port number on which the MySQL server
+        is listening (default is 3306 for MySQL).
+        user: The username for authenticating with the MySQL server.
+        passwd: The password for authenticating with the MySQL server.
+        db: The name of the MySQL database to connect to.
+        charset: The character set to use for communication
+        with the MySQL database (default is "utf8").'''
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+
+    for state in states:
+        if state[1][0] == "N":
+            print(state)
+    cur.close()
+    conn.close()
