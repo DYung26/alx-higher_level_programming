bb49a219cbcd1da4287fcfc6eefdee332c64abd0
DYung26 <theadstore1407@gmail.com>
2024-04-15 07:48:31 -0700

    a script that takes in arguments and displays all values in the `states` table of `hbtn_0e_0_usa` where `name` matches the argument. But this time, it is safe from MySQL injections!

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''Filter states by user input'''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    '''Establish a connection to the MySQL database.
+    Parameters:
+        host: The host name where the MySQL server is running.
+        port: The port number on which the MySQL server
+            is listening (default is 3306 for MySQL).
+        user: The username for authenticating with the MySQL server.
+        passwd: The password for authenticating with the MySQL server.
+        db: The name of the MySQL database to connect to.
+        charset: The character set to use for communication
+            with the MySQL database (default is "utf8").'''
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}'"
+        " ORDER BY id ASC", (sys.argv[4],)
+    )
+    states = cur.fetchall()
+
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
