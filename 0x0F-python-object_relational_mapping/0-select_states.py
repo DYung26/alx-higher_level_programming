85d827b89ebb12c4d19877cd060001ad1e07d3dd
DYung26 <theadstore1407@gmail.com>
2024-04-15 05:41:46 -0700

    a script that lists all `states` from the database `hbtn_0e_0_usa` taking `m ysql username`, `mysql password` and `database name` as arguments.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,27 +2,28 @@
 import MySQLdb
 import sys
 
-# Establish a connection to the MySQL database.
-# Parameters:
-#   host: The host name where the MySQL server is running.
-#   port: The port number on which the MySQL server is listening (default is 3306 for MySQL).
-#   user: The username for authenticating with the MySQL server.
-#   passwd: The password for authenticating with the MySQL server.
-#   db: The name of the MySQL database to connect to.
-#   charset: The character set to use for communication with the MySQL database (default is "utf8").
-conn = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user=sys.argv[1],
-    passwd=sys.argv[2],
-    db=sys.argv[3],
-    charset="utf8"
-)
-cur = conn.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC")
-states = cur.fetchall()
+if __name__ == '__main__':
+    # Establish a connection to the MySQL database.
+    # Parameters:
+    #   host: The host name where the MySQL server is running.
+    #   port: The port number on which the MySQL server is listening (default is 3306 for MySQL).
+    #   user: The username for authenticating with the MySQL server.
+    #   passwd: The password for authenticating with the MySQL server.
+    #   db: The name of the MySQL database to connect to.
+    #   charset: The character set to use for communication with the MySQL database (default is "utf8").
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
 
-for state in states:
-    print(state)
-cur.close()
-conn.close()
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
