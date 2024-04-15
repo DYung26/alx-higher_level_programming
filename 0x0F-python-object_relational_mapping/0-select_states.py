b4b0e8ced0fc737c6d43fe35d5912aab832f53d3
DYung26 <theadstore1407@gmail.com>
2024-04-15 05:32:24 -0700

    a script that lists all `states` from the database `hbtn_0e_0_usa` taking `mysql username`, `mysql password` and `database name` as arguments.

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
+conn = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=sys.argv[1],
+    passwd=sys.argv[2],
+    db=sys.argv[3],
+    charset="utf8"
+)
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC")
+states = cur.fetchall()
+
+for state in states:
+    print(state)
+cur.close()
+conn.close()
