3e1562bb02abf101ff543ed75ded4999a03aeaec
DYung26 <theadstore1407@gmail.com>
2024-04-15 11:14:24 -0700

    a script that takes in the name of a state as an argument and lists all `cities` of that state, using the database `hbtn_0e_4_usa`.

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''Filter states by user input'''
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    '''Establish a connection to the MySQL database.
+    Parameters:
+        host: The host name where the MySQL server is running.
+        port: The port number on which the MySQL server
+            is listening (default is 3306 for MySQL).
+        user: The username for authenticating with the MySQL server.
+        passwd: The password for authenticating with the MySQL server.
+        db: The name of the MySQL database to connect to.
+        charset: The character set to use for communication
+            with the MySQL database (default is "utf8").'''
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT cities.name FROM cities WHERE cities.state_id = "
+        "(SELECT id FROM states WHERE name = \"Texas\")"
+    )
+    state_cities = cur.fetchall()
+    city_list = []
+    for city in state_cities:
+        city_list.append(city[0])
+    print(', '.join(city_list))
+    cur.close()
+    conn.close()
