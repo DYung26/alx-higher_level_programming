65268c4bc021b4ae8a67526368edfe51fa3290ab
DYung26 <theadstore1407@gmail.com>
2024-04-15 21:27:47 -0700

    improved `model_city.py`, but with No change.

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""First state model"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_state import Base
+
+# Base = declarative_base()
+
+
+class City(Base):
+    """
+    Represents a state entity in the database.
+
+    Attributes:
+        __tablename__ (str): The name of the table in the database
+            associated with this model.
+        id (Column): An auto-incrementing integer primary key column
+            representing the unique identifier of the state.
+        name (Column): A string column representing the name of the state.
+    """
+    __tablename__ = "cities"
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
