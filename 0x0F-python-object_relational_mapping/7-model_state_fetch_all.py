7881c1dbda45b1bd84c69e3558ec10d3e2d035a6
DYung26 <theadstore1407@gmail.com>
2024-04-15 17:58:38 -0700

    a script that lists all `State` objects from the database `hbtn_0e_6_usa`.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     engine = create_engine(
-         'mysql+mysqldb://{}:{}@localhost/{}'.format(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
             sys.argv[1],
             sys.argv[2],
             sys.argv[3]
