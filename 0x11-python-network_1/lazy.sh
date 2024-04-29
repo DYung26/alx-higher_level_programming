33729a4b024b87da81bf0393932057ba7d0e5e9c
DYung26 <theadstore1407@gmail.com>
2024-04-29 04:23:14 -0700

    created to automate staging and pushing of files to git.

--- 0x11-python-network_1/lazy.sh
+++ 0x11-python-network_1/lazy.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 echo "Enter file name:"
-read file_name
+read -e file_name # `-e` option to enable line editing
 
-git add "${file_name}"
+git add "$file_name"
 
 echo "Enter Your Commit Message:"
-read commit_msg
+# read -e commit_msg
 
-git commit -m "${commit_msg}"
+echo "Enter multiline input. Type 'END' on a new line to finish:"
+commit_msg=""
+while IFS= read -e -r line && [ "$line" != "END" ]; do
+    commit_msg+="$line"$'\n'
+done
+
+echo "Multiline input received:"
+echo "$commit_msg"
+
+
+git commit -m "$commit_msg"
 
 git push
