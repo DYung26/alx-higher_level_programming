bf92e4da3dffafd8595b66ebfea9ca0015f7b441
DYung26 <theadstore1407@gmail.com>
2024-04-29 05:05:29 -0700

    a Python script that fetches `https://alx-intranet.hbtn.io/status` using the package `urllib`

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+''' Uses `urllib` to fetch `https://alx-intranet.hbtn.io/status'''
+
+import urllib.request as urllib_req
+if __name__ == '__main__':
+    with urllib_req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content_bytes = response.read()
+        print(f"""Body response:
+            - type: {type(content_bytes)}
+            - content: {content_bytes}
+            - utf8 content: {content_bytes.decode('utf-8')}""")
