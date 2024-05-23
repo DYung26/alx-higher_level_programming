5738c8e99e5b21b730b2d6c2f88de4c3c9a48604
DYung26 <theadstore1407@gmail.com>
2024-05-22 21:46:01 -0700

    `https://jsonplaceholder.typicode.com/todos` - a script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+request(`${args[2]}`, (error, response, body) => {
+  if (error) {
+    return;
+  }
+  const todosList = JSON.parse(response.body);
+  const idCtask = {};
+  for (let i = 0; i < todosList.length; i++) {
+    if (!idCtask[todosList[i].userId]) {
+      idCtask[todosList[i].userId] = 0;
+    }
+    if (todosList[i].completed) {
+      idCtask[todosList[i].userId]++;
+    }
+  }
+  console.log(idCtask);
+});
